175e7c8efd4524f3324feebf05a7e5cdd4f0bf43
CamilloFilippo <kamilfkozlowski@gmail.com>
2015-04-22 13:31:15 +0200

    with first function

--- FirstOpenCV/main.cpp
+++ FirstOpenCV/main.cpp
@@ -6,6 +6,9 @@
 
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
 #include <cv.h>
 
 
@@ -105,17 +108,50 @@ Mat ColorDetector::process(const Mat &image)
 	return result;
 }
 
+static void read_csv(const string& filename, vector<Mat>& images, vector<int> &labels, char separator = ';')
+{
+	std::ifstream file(filename.c_str(), ifstream::in);
+
+	if(!file)
+	{
+		string error_message = "No valid input file was given, please check the given filename.";
+		CV_Error(CV_StsBadArg,error_message);
+	}
+
+	string line, path, classlabel;
+
+	while(getline(file,line)){
+		stringstream liness(line);
+		getline(liness,path, separator);
+		getline(liness,classlabel);
+
+		if(!path.empty() && !classlabel.empty())
+		{
+			images.push_back(imread(path,0));
+			labels.push_back(atoi(classlabel.c_str()));
+		}
+	}
+
+
+
+}
+
+
+
 void main()
 {
 	
-	ColorDetector cdetect;
-	Mat image = imread("E:\\lena.png",CV_LOAD_IMAGE_COLOR);
-	
-	cdetect.setTargetColor(130,190,230);
+	//ColorDetector cdetect;
+	//Mat image = imread("E:\\lena.png",CV_LOAD_IMAGE_COLOR);
+	//
+	//cdetect.setTargetColor(130,190,230);
 
-	namedWindow("result");
-	imshow("result", cdetect.process(image));
-	waitKey();
+	//namedWindow("result");
+	//imshow("result", cdetect.process(image));
+	//waitKey();
+
+	
+
 
 }
 
